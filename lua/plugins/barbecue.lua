6b5f8e8cd8979208bb710a90d37ed0b4b43fb267
olivier <olibitter@gmail.com>
2023-10-29 18:48:15 +0100

    Add barbecue and navic

---
+++ lua/plugins/barbecue.lua
@@ -0,0 +1,18 @@
+return {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+        "SmiteshP/nvim-navic",
+        "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+        theme = {
+            dirname = { fg = "#737aa2" },
+            -- dirname = { link = "SpecialKey" },
+            basename = { link = "String", bold = true },
+            context = { link = "Function" },
+            separator = { link = "barbecue_dirname" }
+        }
+    },
+}
