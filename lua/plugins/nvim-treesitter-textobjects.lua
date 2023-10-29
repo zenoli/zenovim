6ec21710f4affaa39e90a6ad72cafa4c0704d337
olivier <olibitter@gmail.com>
2023-10-29 10:04:38 +0100

    Cleanup textobjects

--- lua/plugins/nvim-treesitter-textobjects.lua
+++ lua/plugins/nvim-treesitter-textobjects.lua
@@ -27,13 +27,11 @@ return {
                 enable = true,
                 swap_next = {
                     ["<leader>na"] = "@parameter.inner", -- swap parameters/argument with next
-                    ["<leader>n:"] = "@property.outer",  -- swap object property with next
-                    ["<leader>nm"] = "@function.outer",  -- swap function with next
+                    ["<leader>nf"] = "@function.outer",  -- swap function with next
                 },
                 swap_previous = {
                     ["<leader>pa"] = "@parameter.inner", -- swap parameters/argument with prev
-                    ["<leader>p:"] = "@property.outer",  -- swap object property with prev
-                    ["<leader>pm"] = "@function.outer",  -- swap function with previous
+                    ["<leader>pf"] = "@function.outer",  -- swap function with previous
                 },
             },
             move = {
