9f69baedca47ad42327cb67550fa30597b63745a
olivier <olibitter@gmail.com>
2023-10-29 18:44:38 +0100

    Update lualine

--- lua/plugins/nvim-cmp/init.lua
+++ lua/plugins/nvim-cmp/init.lua
@@ -13,7 +13,7 @@ return {
     },
     opts = function()
         vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-        local cmp = require("cmp")
+        local cmp = require "cmp"
         local defaults = require("cmp.config.default")()
         return {
             completion = {
@@ -21,37 +21,37 @@ return {
             },
             snippet = {
                 expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
+                    require "luasnip".lsp_expand(args.body)
                 end,
             },
             window = {
                 completion = cmp.config.window.bordered(),
                 documentation = cmp.config.window.bordered(),
             },
-            mapping = cmp.mapping.preset.insert({
-                ["<c-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-                ["<c-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+            mapping = cmp.mapping.preset.insert {
+                ["<c-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+                ["<c-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
                 ["<c-k>"] = cmp.mapping.scroll_docs(-4),
                 ["<c-j>"] = cmp.mapping.scroll_docs(4),
                 ["<c-space>"] = cmp.mapping.complete(),
                 ["<c-e>"] = cmp.mapping.abort(),
-                ["<cr>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                ["<s-cr>"] = cmp.mapping.confirm({
+                ["<cr>"] = cmp.mapping.confirm { select = true },  -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ["<s-cr>"] = cmp.mapping.confirm {
                     behavior = cmp.ConfirmBehavior.Replace,
                     select = true,
-                }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-            }),
-            sources = cmp.config.sources({
+                },  -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            },
+            sources = cmp.config.sources {
                 { name = "nvim_lsp" },
                 { name = "luasnip" },
                 { name = "buffer" },
                 { name = "path" },
                 { name = "git" },
                 { name = "calc" },
-            }),
+            },
             formatting = {
                 format = function(entry, vim_item)
-                    local icons = require("plugins.nvim-cmp.icons")
+                    local icons = require "plugins.nvim-cmp.icons"
                     if icons[vim_item.kind] then
                         if entry.source.name == "calc" then
                             vim_item.menu = "[Calc]"
@@ -80,11 +80,11 @@ return {
         }
     end,
     config = function(_, opts)
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-        local cmp = require("cmp")
+        local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+        local cmp = require "cmp"
         cmp.setup(opts)
         cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-        require("cmp_git").setup({ filetypes = { "gitcommit", "octo", "markdown" } })
+        require "cmp_git".setup { filetypes = { "gitcommit", "octo", "markdown" } }
     end,
 }
