880d5f288126b00e398774be3c5c85fc33b20e73
olivier <olibitter@gmail.com>
2023-10-29 10:49:20 +0100

    Switch to nvim-tmux-navigator

---
+++ lua/plugins/nvim-tmux-navigator.lua
@@ -0,0 +1,12 @@
+return {
+    "alexghergh/nvim-tmux-navigation",
+    opts = {
+        disable_when_zoomed = true,
+        keybindings = {
+            left = "<a-h>",
+            down = "<a-j>",
+            up = "<a-k>",
+            right = "<a-l>"
+        }
+    }
+}
