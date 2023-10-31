146488425f218c7c703569de0bd36e255a7c0576
olivier <olibitter@gmail.com>
2023-10-31 21:53:26 +0100

    Use slanted lualine separators

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,21 +9,25 @@ return {
                 winbar = {},
             },
             ignore_focus = { "neo-tree", "calendar" },
+            -- These symbols can be found by searching for "e0b" in
+            -- https://www.nerdfonts.com/cheat-sheet
+            component_separators = { left = "", right = "" },
+            section_separators = { left = "", right = "" },
         },
         sections = {
             lualine_b = { "branch" },
             lualine_c = {
                 {
-                    'filename',
+                    "filename",
                     file_status = true, -- Displays file status (readonly status, modified status)
                     newfile_status = true, -- Display new file status (new file means no write after created)
                     path = 0, -- 0: Just the filename
                     shorting_target = 40, -- Shortens path to leave 40 spaces in the window
                     symbols = {
-                        modified = ' ', -- Text to show when the file is modified.
-                        readonly = ' ', -- Text to show when the file is non-modifiable or readonly.
-                        unnamed = '[No Name]', -- Text to show for unnamed buffers.
-                        newfile = '󰛄 ', -- Text to show for newly created file before first write
+                        modified = " ", -- Text to show when the file is modified.
+                        readonly = " ", -- Text to show when the file is non-modifiable or readonly.
+                        unnamed = " ", -- Text to show for unnamed buffers.
+                        newfile = "󰛄 ", -- Text to show for newly created file before first write
                     },
                 },
             },
