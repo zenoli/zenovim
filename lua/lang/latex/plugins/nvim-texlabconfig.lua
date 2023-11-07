74be407fcc2fe20bf7d0106d7902437c2e8c68cb
olivier <olibitter@gmail.com>
2023-11-07 22:06:09 +0100

    Add autocommand to setup texlab bindings

--- lua/lang/latex/plugins/nvim-texlabconfig.lua
+++ lua/lang/latex/plugins/nvim-texlabconfig.lua
@@ -3,4 +3,16 @@ return {
     ft = "tex",
     build = "go build",
     opts = {},
+    init = function()
+        vim.api.nvim_create_autocmd({ "BufEnter" }, {
+            pattern = { "*.tex", "*.bibtex" },
+            group = vim.api.nvim_create_augroup("Texlabconfig", {}),
+            desc = "Define buffer-local texlab mappings.",
+            callback = function(ev)
+                local map = require("config.utils").getMapFn { buffer = ev.buf }
+                map("n", "<localleader>f", "<cmd>TexlabForward<cr>", { desc = "Texlab forward search"})
+                map("n", "<localleader>b", "<cmd>TexlabBuild<cr>", { desc = "Texlab build"})
+            end,
+        })
+    end,
 }
