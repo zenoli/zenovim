57783fb318d2e92fc3221790082a28370412b517
olivier <olibitter@gmail.com>
2023-11-07 21:06:59 +0100

    Update fidget config

--- lua/plugins/lsp/aux/fidget.lua
+++ lua/plugins/lsp/aux/fidget.lua
@@ -2,5 +2,16 @@ return {
     "j-hui/fidget.nvim",
     tag = "legacy",
     event = "LspAttach",
-    opts = {},
+    opts = {
+        timer = {
+            spinner_rate = 125,  -- frame rate of spinner animation, in ms
+            fidget_decay = 1000, -- how long to keep around empty fidget, in ms
+            task_decay = 500,   -- how long to keep around completed task, in ms
+        },
+        sources = {
+            ["jdtls"] = {
+                ignore = true,
+            },
+        },
+    },
 }
