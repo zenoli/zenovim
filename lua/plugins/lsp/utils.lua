46d13ef9c0b0720a8d8bd5dcee15505ccdf63541
olivier <olibitter@gmail.com>
2023-11-07 20:46:36 +0100

    Refactor lsp settings

--- lua/plugins/lsp/utils.lua
+++ lua/plugins/lsp/utils.lua
@@ -1,16 +1,12 @@
 local M = {}
 
-function M.setup_keybindings()
+function M.register_on_attach(callback)
     vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("UserLspConfig", {}),
         callback = function(attachEvent)
             local client = vim.lsp.get_client_by_id(attachEvent.data.client_id)
             local buf = attachEvent.buf
-            require "plugins.lsp.keybindings".setup(buf)
-            if client.server_capabilities.documentSymbolProvider then
-                require("nvim-navic").attach(client, buf)
-                require("nvim-navbuddy").attach(client, buf)
-            end
+            callback(client, buf)
         end,
     })
 end
@@ -21,26 +17,24 @@ function M.setup_diagnostics(opts)
         vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
     end
     vim.diagnostic.config(opts.config)
+
+    local border = opts.config.float.border
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+        vim.lsp.handlers.hover,
+        { border = border, silent = true }
+    )
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+        vim.lsp.handlers.signature_help,
+        { border = border }
+    )
 end
 
-function M.setup_default_config(opts)
+function M.setup_default_config()
     local lspconfig = require "lspconfig"
     lspconfig.util.default_config = vim.tbl_deep_extend(
         "force",
         lspconfig.util.default_config,
-        {
-            handlers = {
-                ["textDocument/hover"] = vim.lsp.with(
-                    vim.lsp.handlers.hover,
-                    { border = opts.border, silent = true }
-                ),
-                ["textDocument/signatureHelp"] = vim.lsp.with(
-                    vim.lsp.handlers.signature_help,
-                    { border = opts.border }
-                ),
-            },
-            capabilities = require "cmp_nvim_lsp".default_capabilities(),
-        }
+        { capabilities = require "cmp_nvim_lsp".default_capabilities() }
     )
 end
 
