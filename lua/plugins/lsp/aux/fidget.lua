7f3221f10b4a90de9ebd5c56e0981e82e7ee522d
olivier <olibitter@gmail.com>
2023-11-28 12:08:28 +0100

    Fix fidget options

--- lua/plugins/lsp/aux/fidget.lua
+++ lua/plugins/lsp/aux/fidget.lua
@@ -2,9 +2,9 @@ return {
     "j-hui/fidget.nvim",
     event = "LspAttach",
     opts = {
-        suppress_on_insert = true,   -- Suppress new messages while in insert mode
-        ignore_done_already = false, -- Ignore new tasks that are already complete
         progress = {
+            suppress_on_insert = true,   -- Suppress new messages while in insert mode
+            ignore_done_already = false, -- Ignore new tasks that are already complete
             ignore = { "jdtls" },
         },
     },
