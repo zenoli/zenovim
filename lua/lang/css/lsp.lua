c4d425dc5ed5af02e403ca2c829cadd65db599cb
Bitter Olivier <olivier.bitter@ti8m.ch>
2024-04-23 23:01:30 +0200

    Use prettier_d in css files

--- lua/lang/css/lsp.lua
+++ lua/lang/css/lsp.lua
@@ -1,11 +1,12 @@
+local prettier_d = require("efmls-configs.formatters.prettier_d")
 return {
     "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-        return vim.tbl_deep_extend("force", opts, {
-            servers = {
-                cssls = {},
-                tailwindcss = {},
-            },
-        })
-    end,
+    dependencies = {
+        "creativenull/efmls-configs-nvim",
+    },
+    opts = {
+        efm = {
+            css = function() return { prettier_d } end,
+        },
+    },
 }
