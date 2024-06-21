4be4aca444942bc9e9739e4aea569241a4561039
olivier <olibitter@gmail.com>
2024-06-21 22:16:38 +0200

    Enable commenting jsx/tsx

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,11 @@
 return {
+    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
     "numToStr/Comment.nvim",
     keys = { "gcc", "gco", "gcO", "gcb", { "gc", "S", mode = "x" } },
-    opts = {},
+    config = function(_, opts)
+        require("Comment").setup {
+            -- Enable commenting tsx
+            pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+        }
+    end,
 }
